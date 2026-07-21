025fd027d94c78ba102778b7d854bd57c6243102
isoneofficial <isanbabu0202@gmail.com>
2026-07-22 05:54:45 +0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,31 @@
+import { auth } from "./firebase.js";
 
+import {
+
+  signInWithEmailAndPassword
+
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", async () => {
+
+  const email = document.getElementById("email").value;
+
+  const password = document.getElementById("password").value;
+
+  try {
+
+    await signInWithEmailAndPassword(auth, email, password);
+
+    alert("Login Successful!");
+
+    window.location.href = "index.html";
+
+  } catch (error) {
+
+    alert(error.message);
+
+  }
+
+});
