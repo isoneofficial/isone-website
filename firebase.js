bcef9f29144e04540bdaf21b3255646c22806b0d
isoneofficial <isanbabu0202@gmail.com>
2026-07-22 05:26:04 +0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDChtZ53ffjH6w10tTF8NS5VbW0lWmJpjk",
+
+  authDomain: "is-one-36ebc.firebaseapp.com",
+
+  projectId: "is-one-36ebc",
+
+  storageBucket: "is-one-36ebc.firebasestorage.app",
+
+  messagingSenderId: "446720779948",
+
+  appId: "1:446720779948:web:c71b5f98d347e21647a109",
+
+  measurementId: "G-QX9LVCHFFE"
+
+};
