213d62e6da594bc14b54414b070f2f7832fdb478
isoneofficial <isanbabu0202@gmail.com>
2026-07-22 07:56:27 +0600

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,89 @@
+import { auth } from "./firebase.js";
 
+import {
+
+  RecaptchaVerifier,
+
+  signInWithPhoneNumber
+
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// Create reCAPTCHA
+
+window.recaptchaVerifier = new RecaptchaVerifier(
+
+  auth,
+
+  "recaptcha-container",
+
+  {
+
+    size: "normal"
+
+  }
+
+);
+
+window.recaptchaVerifier.render();
+
+// Send OTP
+
+window.sendOTP = async function () {
+
+  let phone = document.getElementById("phone").value.trim();
+
+  if (!phone.startsWith("+")) {
+
+    phone = "+88" + phone;
+
+  }
+
+  try {
+
+    const confirmationResult =
+
+      await signInWithPhoneNumber(
+
+        auth,
+
+        phone,
+
+        window.recaptchaVerifier
+
+      );
+
+    window.confirmationResult = confirmationResult;
+
+    alert("OTP Sent!");
+
+    document.getElementById("otpBox").style.display = "block";
+
+  } catch (e) {
+
+    alert(e.message);
+
+  }
+
+};
+
+// Verify OTP
+
+window.verifyOTP = async function () {
+
+  const otp = document.getElementById("otp").value;
+
+  try {
+
+    await window.confirmationResult.confirm(otp);
+
+    alert("Login Successful");
+
+    window.location.href = "dashboard.html";
+
+  } catch (e) {
+
+    alert("Invalid OTP");
+
+  }
+
+};
