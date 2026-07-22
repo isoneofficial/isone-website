997f880c8ab50b182914a8043454e273aaa43872
isoneofficial <isanbabu0202@gmail.com>
2026-07-22 06:07:38 +0600

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,35 @@
+import { auth } from "./firebase.js";
 
+import {
+
+onAuthStateChanged,
+
+signOut
+
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const username=document.getElementById("username");
+
+const logoutBtn=document.getElementById("logoutBtn");
+
+onAuthStateChanged(auth,(user)=>{
+
+if(user){
+
+username.innerHTML="Welcome, "+(user.displayName||user.email);
+
+}else{
+
+window.location.href="login.html";
+
+}
+
+});
+
+logoutBtn.addEventListener("click",async()=>{
+
+await signOut(auth);
+
+window.location.href="login.html";
+
+});
