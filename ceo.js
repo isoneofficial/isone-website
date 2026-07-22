dbc331332631b69f75e25e5becf202ac616eb446
isoneofficial <isanbabu0202@gmail.com>
2026-07-22 09:38:57 +0600

    Update ceo.js

--- ceo.js
+++ ceo.js
@@ -1,1 +1,23 @@
+// ceo.js
 
+function ceoLogin() {
+
+    const username = document.getElementById("username").value.trim();
+
+    const password = document.getElementById("password").value;
+
+    // CEO Login
+
+    if (username === "IS ONE CEO" && password === "123456") {
+
+        alert("Welcome CEO!");
+
+        window.location.href = "dashboard.html";
+
+    } else {
+
+        alert("Wrong Username or Password!");
+
+    }
+
+}
